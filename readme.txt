b897e0f28f0513a8462295652ce8c495eb7ee0f9
ndmscorp <ndmscorp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-23 15:55:45 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/ndms-epay-plus-for-woocommerce/tags/1.0.2@1097312 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== NDMS ePay Plus ===
+Contributors: ndmscorp, growdev
+Donate link: http://www.ndmscorp.com/
+Tags: woocommerce, ecommerce, payment gateway
+Requires at least: 3.5
+Tested up to: 4.1
+Stable tag: 1.0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Accept Credit Cards. Simply.™ with NDMS ePay Plus for WooCommerce
+
+== Description ==
+
+NDMS ePay Plus is a complete payment processing solution for merchants using WooCommerce to power their eCommerce Store. ePay Plus includes everything you need to accept credit and debit cards at checkout.
+
+The customer enters their credit card details (Card Holder Name, Credit Card Number, Expiration Date, CVV) and NDMS ePay Plus handles authorization and settlement. The customer stays on your site for a seamless checkout experience.
+
+== Configuring ePay+ settings in the WooCommerce admin area ==
+
+ * Enable/Disable - Enable or disable this gateway from being used on the site.
+ * Title - This is the title that appears on the checkout page for this payment gateway.
+ * Description - This setting controls the message that appears under the payment fields on the checkout page. Here you can list the types of cards you accept.
+ * Transaction Description -
+ * ePay+ Secure ID - ePay+ Secure ID is generated in the Merchant Console.  See below.
+ * Payment Type - Which payment command to run: Sale authorizes and charges the card. Authorize Only verifies funds only.
+ * Test Mode - If checked the transaction will be simulated by ePay+, but not actually processed.
+ * Sandbox - If checked the sandbox server will be used.  This overrides the gateway URL parameter below.
+ * Gateway URL Override - Override for URL of ePay+
+ * CVV - If checked the CVV field is displayed to the customer and required.
+ * Debugging - Receive emails containing the data sent to and from ePay+. Does not include credit card number.
+ * Debugging Email - Email of recipient of debug emails.
+
+ Press "Save changes" to apply your changes.
+
+
+== NDMS ePay+ Secure ID ==
+
+ * To use your ePay+ payment gateway for live transactions you will need your unique ePay+ Secure Id. 
+ * The secure ID can be found in the NDMS Welcome email along with Login instructions for the Merchant Console.
+ * If you don't have an NDMS ePay+ Account please visit our website http://epayplus.ndmscorp.com 
+ * Or Call 424.247.5511 to speak to one of our team members direct.
+
+
+
+== Installation ==
+
+ * Unzip the files and upload the folder into your plugins folder (wp-content/plugins/) overwriting old versions if they exist
+ * Activate the plugin in your WordPress admin area.
+ * Open the settings page for WooCommerce and click the "Payment Gateways" tab
+ * Click on the sub tab for "ePay+"
+ * Configure your ePay+ express settings.  See below how to.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.2 =
+* Adding compatibility with WooCommerce Subscriptions
+
+= 1.0.1 =
+* Adjusting the PIN parameter
+
+= 1.0.0 =
+* First Version
+
+== Upgrade Notice ==
+
+
---
+++ woocommerce-gateway-epayplus.php
@@ -0,0 +1,114 @@
+<?php
+/*
+Plugin Name: WooCommerce ePay+ Gateway
+Plugin URI: http://ndmscorp.com/epay-plus/
+Description: Extends WooCommerce with a <a href="http://www.ndmscorp.com/epay-plus/" target="_blank">ePay+</a> gateway. A ePay+ gateway account, and a server with SSL support and an SSL certificate is required for security reasons.
+Author: NDMSCorp
+Author URI: http://ndmscorp.com/epay-plus/
+Version: 1.0.2
+Text Domain: wc-gateway-epayplus
+Domain Path: /languages/
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+
+/**
+ * Growdev Order Fulfillment Main Class
+ *
+ * @package  Growdev Order Fulfillment
+ */
+
+class WC_Epayplus {
+
+	protected static $instance = null;
+
+	/**
+	 *  Constructor
+	 */
+	function __construct() {
+
+		if ( class_exists( 'WooCommerce' ) ) {
+
+			include_once( 'includes/class-wc-gateway-epayplus.php' );
+
+			if ( class_exists( 'WC_Subscriptions_Order' ) ) {
+				include_once( 'includes/class-wc-gateway-epayplus-subscriptions.php' );
+			}
+
+			load_plugin_textdomain( 'wc-gateway-epayplus', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+			define( 'EPAYPLUS_DIR', WP_PLUGIN_DIR . "/" . plugin_basename( dirname(__FILE__) ) . '/' );
+			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_links' ) );
+			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_epayplus_gateway') );
+
+		} else {
+
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+
+		}
+
+	}
+
+	/**
+	 * Plugin page links
+	 *
+	 * @param array $links
+	 * @return array
+	 */
+	function plugin_links( $links ) {
+
+		$plugin_links = array(
+			'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_gateway_epayplus' ) . '">' .
+			__( 'Settings', 'wc-gateway-epayplus' ) . '</a>',
+			'<a href="http://www.ndmscorp.com/epay-plus/">' . __( 'Docs', 'wc-gateway-epayplus' ) . '</a>',
+		);
+
+		return array_merge( $plugin_links, $links );
+	}
+
+	/**
+	 * Add the gateway to woocommerce
+	 *
+	 * @param array $methods
+	 * @return array
+	 **/
+	function add_epayplus_gateway( $methods ) {
+
+		if ( class_exists( 'WC_Subscriptions_Order' ) ) {
+			$methods[] = 'WC_Gateway_Epayplus_Subscriptions';
+		} else {
+			$methods[] = 'WC_Gateway_Epayplus';
+		}
+
+		return $methods;
+	}
+
+	/**
+	 * Start the Class when called
+	 *
+	 * @return WC_Epayplus
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @return string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error"><p>' . sprintf( __( 'WooCommerce ePay+ requires %s to be installed and active.', 'wc-gateway-epayplus' ), '<a href="http://woocommerce.com/" target="_blank">' . __( 'WooCommerce', 'wc-gateway-epayplus' ) . '</a>' ) . '</p></div>';
+	}
+
+
+
+}
+
+add_action( 'plugins_loaded', array( 'WC_Epayplus', 'get_instance' ) );
